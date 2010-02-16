314afe4167971014a96483e106a9a35a6e79d527
gwoo <gwoohoo@gmail.com>
2010-02-16 08:53:51 -0800

    updating bot style and layout

--- views/layouts/default.html.php
+++ views/layouts/default.html.php
@@ -3,15 +3,14 @@
 <head>
 	<?php echo $this->html->charset(); ?>
 	<title>Lithium Bot <?php echo $this->title(); ?></title>
-	<?php echo $this->html->style(array('lithium', '/li3_bot/css/li3_bot')); ?>
+	<?php echo $this->html->style(array('lithium', 'u1m', '/li3_bot/css/li3_bot')); ?>
 	<?php echo $this->scripts(); ?>
 	<?php echo $this->html->link('Icon', null, array('type' => 'icon')); ?>
 </head>
 <body class="bot">
 	<div id="container">
 		<div id="header">
-			<h1>Lithium</h1>
-			<h2>Bot</h2>
+			<h1>Lithium Bot</h1>
 			<ul class="crumbs">
 			<?php foreach ($breadcrumbs as $link => $title): ?>
 				  <li><?php echo ($link != '#') ? $this->html->link($title, $link) : $title; ?></li>
@@ -22,5 +21,19 @@
 			<?php echo $this->content; ?>
 		</div>
 	</div>
+	<div id="footer-spacer"></div>
+</div>
+<div id="footer">
+	<p class="copyright">Pretty much everything is &copy; 2009 and beyond, the Union of Rad</p>
+</div>
+<?php echo $this->html->script(array(
+	'http://code.jquery.com/jquery-1.4.1.min.js',
+	'rad.cli',
+)); ?>
+<script type="text/javascript" charset="utf-8">
+	$(document).ready(function () {
+		RadCli.setup();
+	});
+</script>
 </body>
 </html>
