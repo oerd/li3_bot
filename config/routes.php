ee74f078086cb9c332cc41cce3ae141e585759c2
David Persson <davidpersson@gmx.de>
2011-01-05 01:31:39 +0100

    Reverting removal of Locale route.
    This will probably be changed later when the enclosing app changes.

--- config/routes.php
+++ config/routes.php
@@ -3,23 +3,35 @@
 use \lithium\net\http\Router;
 use \app\extensions\route\Locale;
 
-Router::connect('/bot', array(
-	'library' => 'li3_bot', 'controller' => 'pages', 'action' => 'home'
-));
-Router::connect('/bot/logs', array(
-	'library' => 'li3_bot', 'controller' => 'logs', 'action' => 'index'
-));
-Router::connect('/bot/logs/search/{:channel}', array(
-	'library' => 'li3_bot', 'controller' => 'logs', 'action' => 'search'
-));
-Router::connect('/bot/logs/{:channel}', array(
-	'library' => 'li3_bot', 'controller' => 'logs', 'action' => 'index'
-));
-Router::connect('/bot/logs/{:channel}/{:date:[0-9]{4}-[0-9]{2}-[0-9]{2}}', array(
-	'library' => 'li3_bot', 'controller' => 'logs', 'action' => 'view'
-));
-Router::connect('/bot/tells', array(
-	'library' => 'li3_bot', 'controller' => 'tells', 'action' => 'index'
-));
+Router::connect(new Locale(array(
+	'template' => '/bot',
+	'params' => array(
+		'library' => 'li3_bot', 'controller' => 'pages', 'action' => 'home'
+))));
+Router::connect(new Locale(array(
+	'template' => '/bot/logs',
+	'params' => array(
+		'library' => 'li3_bot', 'controller' => 'logs', 'action' => 'index'
+))));
+Router::connect(new Locale(array(
+	'template' => '/bot/logs/search/{:channel}',
+	'params' => array(
+		'library' => 'li3_bot', 'controller' => 'logs', 'action' => 'search'
+))));
+Router::connect(new Locale(array(
+	'template' => '/bot/logs/{:channel}',
+	'params' => array(
+		'library' => 'li3_bot', 'controller' => 'logs', 'action' => 'index'
+))));
+Router::connect(new Locale(array(
+	'template' => '/bot/logs/{:channel}/{:date:[0-9]{4}-[0-9]{2}-[0-9]{2}}',
+	'params' => array(
+		'library' => 'li3_bot', 'controller' => 'logs', 'action' => 'view'
+))));
+Router::connect(new Locale(array(
+	'template' => '/bot/tells',
+	'params' => array(
+		'library' => 'li3_bot', 'controller' => 'tells', 'action' => 'index'
+))));
 
 ?>
